4614f84dfe567766525a186a11ba01af8a8d5855
inuinseoul <inwoo7233@naver.com>
2021-04-06 20:55:10 +0900

    #2 db connect (mongoDB / mongoose)

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://inupark:*@boilerplate.gwnom.mongodb.net/boilerplate?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })

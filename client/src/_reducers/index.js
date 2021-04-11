52dec6785292b1d22463c3be46cee33d52873a73
inuinseoul <inwoo7233@naver.com>
2021-04-12 00:06:16 +0900

    redux setting end

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import user from './user_reducer';
+
+const rootReducer = combineReducers({
+    // user,
+})
+// reducer들을 합쳐서 하나로 만든다.
+export default rootReducer;
